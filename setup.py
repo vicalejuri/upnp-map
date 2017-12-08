934c00d679ef52c47f8041a44dee6b2d59e0c43f
rainbow corals <barrabin.fc@gmail.com>
2017-12-08 19:37:26 -0200

    Updating upnp-map py package

--- setup.py
+++ setup.py
@@ -3,12 +3,14 @@ from setuptools import setup
 setup(
    name='upnp_map',
    version='0.1',
-   description='Minimal upnp port mapping tool. Useful to allow TCP conections on port X from the internet to this machine',
+   description='Minimal `upnp-map` tool. Useful to allow TCP conections on port X from the internet to this machine',
    author='bbfc',
    author_email='barrabin.fc@gmail.com',
    packages=['upnp_map'],  
    install_requires=['miniupnpc'], 
    scripts=[
        'bin/upnp-map'
-   ]
+   ],
+   keywords=['upnp','port mapping',
+             'firewall-hole','nat-hole','nat-traversal']
 )
