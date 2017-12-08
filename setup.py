aff248df2f358c3ed3d0771eea7c41da4c802a84
rainbow corals <barrabin.fc@gmail.com>
2017-12-08 20:45:00 -0200

    Updating dist packages

--- setup.py
+++ setup.py
@@ -2,17 +2,16 @@ from setuptools import setup
 
 setup(
    name='upnp_map',
-   version='0.1',
+   version='0.1.1',
    description='Minimal `upnp-map` tool. Useful to allow TCP conections on port X from the internet to this machine',
    author='bbfc',
-   author_email='barrabin.fc@gmail.com',
+   author_email='rainbrowcorals@gmail.com',
    url='https://github.com/barrabinfc/upnp-map',
-     download_url = 'https://github.com/barrabinfc/upnp-ma/archive/0.1.tar.gz', # I'll explain this in a second
+# download_url = 'https://github.com/barrabinfc/upnp-map/archive/0.1.tar.gz', # I'll explain this in a second
    packages=['upnp_map'],  
    install_requires=['miniupnpc'], 
    scripts=[
        'bin/upnp-map'
    ],
-   keywords=['upnp','port mapping',
-             'firewall-hole','nat-hole','nat-traversal']
+   keywords=['upnp','port mapping','firewall-hole','nat-hole','nat-traversal']
 )
